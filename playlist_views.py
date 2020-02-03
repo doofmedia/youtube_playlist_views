f5e8dc80b0ff718a5335892282133ac986a86020
ediebold <elliot@diebold.com.au>
2020-02-03 20:52:14 +1100

    New version which does all videos in a channel.

--- playlist_views.py
+++ playlist_views.py
@@ -13,6 +13,10 @@ from google.auth.transport.requests import Request
 
 CLIENT_SECRETS_FILE = 'client_secret.json'
 
+##################################
+# HANDY FUNCTIONS
+##################################
+
 # Login to a specific Google API service.
 def get_service(API_SERVICE_NAME, API_VERSION, SCOPES):
     creds = None
@@ -53,6 +57,9 @@ if __name__ == '__main__':
   # Get the dates from the user.
   start_date = input('Start date (YYYY-MM-DD format): ')
   end_date = input('End date (YYYY-MM-DD format): ')
+  #Hardcoded Dates
+  #start_date = "2020-01-01"
+  #end_date = "2020-01-31"
 
   # Disable OAuthlib's HTTPs verification when running locally.
   # *DO NOT* leave this option enabled when running in production.
@@ -60,54 +67,66 @@ if __name__ == '__main__':
 
   # Login to YT Analytics service
   youtubeAnalytics = get_service('youtubeAnalytics', 'v2', ['https://www.googleapis.com/auth/yt-analytics.readonly'])
-
-  # Pull view count for top 200 videos (maximum allowed for now).
-  # TODO: Switch to using video IDs and pulling the info for every video on the channel.
-  view_results = execute_api_request(
-      youtubeAnalytics.reports().query,
-      ids='channel==MINE',
-      startDate=start_date,
-      endDate=end_date,
-      metrics='views,averageViewPercentage',
-      sort='-views',
-      maxResults=200, # max is 200. 
-      dimensions='video'
-  )
-  
   # Login to main YT service
   youtube = get_service('youtube', 'v3', ['https://www.googleapis.com/auth/youtube.readonly'])
 
-  # Reduce all the ids of the videos into comma separated lists, then fetch their details.
-  ids = [i[0] for i in view_results]
-  chunked_ids = chunks(ids, 50)
+  # Get the channel ID 
+  request = youtube.channels().list(
+    part="contentDetails",
+    mine=True
+  )
+  response = request.execute()
+  channel_uploads_id = response["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
+
+  # Get all videos in the channel, arrange them in a dictionary.
+  video_details = {}
+  nextPageToken = None
+  while True:
+      request = youtube.playlistItems().list(
+            part="snippet,contentDetails",
+            playlistId=channel_uploads_id,
+            maxResults=50,
+            pageToken=nextPageToken
+        )
+      response = request.execute()
+      for video in response["items"]:
+          video_details[video["snippet"]["resourceId"]["videoId"]] = {"title": video["snippet"]["title"] }
+      if "nextPageToken" in response:
+          nextPageToken = response["nextPageToken"]
+      else:
+          break
+
+  # The analytics api has a max of 200 videos we can fetch at once, so we need to chunk the ids into groups.
+  chunked_ids = chunks(list(video_details.keys()), 200)
   id_strings = [",".join(x) for x in chunked_ids]
 
-  video_details = []
+  # Pull view counts for videos
   for id_string in id_strings:
-      request = youtube.videos().list(
-            part="snippet,contentDetails",
-            id=id_string # max is 50
-        ).execute()
-      video_details += request["items"]
-
-
-  # Combine video info (title) with analytics data
-  video_data = {}
-  for result in view_results:
-      video_data[result[0]] = {
-          "title": next(x["snippet"]["title"] for x in video_details if x["id"] == result[0]),
-          "views": result[1],
-          "averageViewPercentage" : result[2],
-          "adjustedViews" : result[1]*result[2]/100
-      }
+      view_results = execute_api_request(
+          youtubeAnalytics.reports().query,
+          ids='channel==MINE',
+          filters='video=='+id_string,
+          startDate=start_date,
+          endDate=end_date,
+          metrics='views,averageViewPercentage',
+          sort='-views',
+          maxResults=200, # max is 200. 
+          dimensions='video'
+      )
+      for result in view_results:
+          video_details[result[0]].update({
+              "views": result[1],
+              "averageViewPercentage" : result[2],
+              "adjustedViews" : result[1]*result[2]/100
+          })
 
   # output a copy of the individual stats to a csv file.
   with open('individual_video_data.csv', mode='w') as csv_file:
     fieldnames = ['title', 'views', 'averageViewPercentage', 'adjustedViews']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     writer.writeheader()
-    for video_datum in video_data:
-        writer.writerow(video_data[video_datum])
+    for video_datum in video_details:
+        writer.writerow(video_details[video_datum])
 
   # Get all the playlists in our channel
   playlists = []
@@ -124,7 +143,7 @@ if __name__ == '__main__':
       if "nextPageToken" in response:
           nextPageToken = response["nextPageToken"]
       else:
-          break;
+          break
 
   # Get all the videos in each playlist, add them to the data structure
   # (since for whatever reason YT doesn't)
@@ -144,16 +163,16 @@ if __name__ == '__main__':
           if "nextPageToken" in response:
               nextPageToken = response["nextPageToken"]
           else:
-              break;
+              break
   
   # Now, gather views by playlist
   playlists_data = []
   for playlist in playlists:
     playlist_adjusted_views = 0
     for video in playlist["videos"]:
-        if video in video_data:
-            playlist_adjusted_views += video_data[video]["adjustedViews"]
-            del video_data[video]
+        if video in video_details and "adjustedViews" in video_details[video]:
+            playlist_adjusted_views += video_details[video]["adjustedViews"]
+            del video_details[video] 
     playlist_data = {
         "title": playlist["snippet"]["title"],
         "adjustedViews" : playlist_adjusted_views
@@ -169,5 +188,14 @@ if __name__ == '__main__':
     for playlist_data in playlists_data:
       writer.writerow(playlist_data)
     writer.writerow({}) # insert a blank row to separate playlists from raw videos.
-    for video_datum in video_data:
-      writer.writerow(video_data[video_datum])
+    
+    # Sort the remaining videos, since we probably don't want to worry about the trash
+    sorted_leftover_videos = []
+    for video in video_details:
+        if 'adjustedViews' in video_details[video]:
+            sorted_leftover_videos.append(video_details[video])
+    sorted_leftover_videos.sort(key=lambda x: -x["adjustedViews"])
+
+    # Output the sorted list into the csv
+    for sorted_leftover_video in sorted_leftover_videos:
+      writer.writerow(sorted_leftover_video)
